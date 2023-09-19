ec5c3a63ab0285a57cfebfc33995bd032fde29b3
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-09-19 19:49:48 +1000

    Feat: App turned into class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Page that lets us increase or decrease
+// How many pokemon are on screen
+// App component needs to:
+//  - Keep track of how many pokemon to show
+// - Iterate over a single, D.R.Y modular Pokemon component
+// - button to increase Pokemon count
+// - button to decrease Pokemon count
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    // start with at least one pokemon on screen
+    this.state = {
+      pokemonCount :1
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Pokemon page</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
